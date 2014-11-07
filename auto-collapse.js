a4382789be8b370d33e99452ea225e9e271f98bd
jasonmj <jasonmj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-07 21:00:15 +0000

    Added Support for Menus too tall for the window
    git-svn-id: https://plugins.svn.wordpress.org/auto-collapse/trunk@1021707 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- auto-collapse.js
+++ auto-collapse.js
@@ -1,27 +1,65 @@
+// WP Admin Sidebar Collapse
 jQuery(document).ready(function($) {
     
-    $("#adminmenuwrap").mouseenter(function() {
-        
-        if ( $(document.body).hasClass('folded') ) {
-            
-            $("#collapse-menu").trigger('click');
-        
-        }
-        
-    });
-    
-    $("#wpbody").mouseover(function() {
-        
+    // Define the functions
+    $adminMenuWrap = $('#adminmenuwrap');
+    var collapseIt = function($adminMenuWrap) {
         if ( !$(document.body).hasClass('folded') ) {
             
             $("#collapse-menu").trigger('click');
+            $(document.body).toggleClass('expanded');
+            $adminMenuWrap.removeClass('floating');
+            $adminMenuWrap.css({'margin-top':'0px'});
         
         }
+    };
+    
+    var expandIt = function($adminMenuWrap) {
+        adminMenuWrap = $('#adminmenuwrap');
+        if ( $(document.body).hasClass('folded') ) {
+            
+            $("#collapse-menu").trigger('click');
+            $(document.body).toggleClass('expanded');
+            if ( $(window).height() > $adminMenuWrap.height() + 28 ) { 
+                $adminMenuWrap.toggleClass('floating');
+            } else {
+                $('.expanded #adminmenuwrap').css({'margin-top':$(window).scrollTop()});
+            }
+            
+        }
         
+    }
+    
+    // Here's where the action is
+    collapseIt($adminMenuWrap);
+    $('#adminmenuwrap, #adminmenuback').hover(function() { expandIt($adminMenuWrap); }); 
+    $('#wpcontent').mouseenter(function() { collapseIt($adminMenuWrap); });
+    
+    // Fancy footwork for menus that don't fit the window height
+    var OldPos = 0;
+    $(window).scroll(function() {
+        if ( $(window).height() < $(this).height() + 28 ) {
+            var NewPos = $(this).scrollTop();
+            if ( NewPos < OldPos && $adminMenuWrap.offset().top > NewPos + 28 ) { // Scrolling Up Above Wrap
+                $('.expanded #adminmenuwrap').css({'margin-top':NewPos});
+            }
+            if ( NewPos < OldPos && $adminMenuWrap.offset().top < NewPos + 28 ) { // Scrolling Up Below Wrap
+                adminMenuWrap.removeClass('floating');
+            }
+            if ( NewPos > OldPos && $adminMenuWrap.offset().top < NewPos + 28) { // Scrolling Down Below Wrap
+                adminMenuWrap.removeClass('floating');
+            }
+            if ( NewPos > OldPos && $adminMenuWrap.offset().top > NewPos + 28 ) { // Scrolling Down Above Wrap
+                adminMenuWrap.removeClass('floating');
+                $('.expanded #adminmenuwrap').css({'margin-top':$(window).scrollTop()});
+            }
+            OldPos = NewPos;
+        }
     });
     
 });
 
+// Customizer Collapse
 jQuery(document).ready(function($) {
     
     $("#customize-controls").mouseenter(function() {

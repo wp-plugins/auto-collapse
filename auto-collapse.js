1c63d075c8cc606a538c80aba481622097a43936
jasonmj <jasonmj@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-11 20:46:55 +0000

    Fixed JS Scrolling Issues
    git-svn-id: https://plugins.svn.wordpress.org/auto-collapse/trunk@1158234 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- auto-collapse.js
+++ auto-collapse.js
@@ -5,29 +5,24 @@ jQuery(document).ready(function($) {
     $adminMenuWrap = $('#adminmenuwrap');
     var collapseIt = function($adminMenuWrap) {
         if ( !$(document.body).hasClass('folded') ) {
-            
             $("#collapse-menu").trigger('click');
             $(document.body).toggleClass('expanded');
-            $adminMenuWrap.removeClass('floating');
-            $adminMenuWrap.css({'margin-top':'0px'});
-        
+            /*$adminMenuWrap.removeClass('floating');*/
+            /*$adminMenuWrap.css({'margin-top':'0px'});*/
         }
     };
     
     var expandIt = function($adminMenuWrap) {
         adminMenuWrap = $('#adminmenuwrap');
         if ( $(document.body).hasClass('folded') ) {
-            
             $("#collapse-menu").trigger('click');
             $(document.body).toggleClass('expanded');
-            if ( $(window).height() > $adminMenuWrap.height() + 28 ) { 
+            /*if ( $(window).height() > $adminMenuWrap.height() + 28 ) { 
                 $adminMenuWrap.toggleClass('floating');
             } else {
                 $('.expanded #adminmenuwrap').css({'margin-top':$(window).scrollTop()});
-            }
-            
+            }*/   
         }
-        
     }
     
     // Here's where the action is
@@ -36,7 +31,7 @@ jQuery(document).ready(function($) {
     $('#wpcontent').mouseenter(function() { collapseIt($adminMenuWrap); });
     
     // Fancy footwork for menus that don't fit the window height
-    var OldPos = 0;
+    /*var OldPos = 0;
     $(window).scroll(function() {
         if ( $(window).height() < $adminMenuWrap.height() + 28 ) {
             var NewPos = $(this).scrollTop();
@@ -55,31 +50,22 @@ jQuery(document).ready(function($) {
             }
             OldPos = NewPos;
         }
-    });
-    
+    });*/
 });
 
 // Customizer Collapse
 jQuery(document).ready(function($) {
     
     $("#customize-controls").mouseenter(function() {
-        
         if ( $('.wp-full-overlay').hasClass('collapsed') ) {
-            
             $(".collapse-sidebar").trigger('click');
-        
         }
-        
     });
     
     $("#customize-preview").mouseover(function() {
-        
         if ( $('.wp-full-overlay').hasClass('expanded') ) {
-            
             $(".collapse-sidebar").trigger('click');
-        
         }
-        
     });
     
 });
